17b0f1829645043e2378b3ae7c4677ca01432ce6
kmmz532 <mizuto1127no01@gmail.com>
2026-01-04 21:07:53 +0900

    chore: setup nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: 'docs',
+  output: 'export',
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
 };
 
 export default nextConfig;
